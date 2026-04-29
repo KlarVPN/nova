747a1cdd4c9e4ec5dd343f414945b90dacf0e90c
Mikhail Gusev <majikx.prod0@gmail.com>
2026-04-29 18:11:15 +0500

    feat: conditionally display BottomNav based on route name

--- webapp/src/components/layout/AppLayout.vue
+++ webapp/src/components/layout/AppLayout.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import BottomNav from './BottomNav.vue'
+
+const route = useRoute()
+const showBottomNav = computed(() => route.name !== 'plans')
 </script>
 
 <template>
   <div class="relative h-dvh w-dvw overflow-hidden bg-[#010101]">
     <div
-      class="h-dvh overflow-y-auto p-3 pb-16 [-webkit-overflow-scrolling:touch] [scrollbar-width:none]"
+      class="h-dvh overflow-y-auto p-3 [-webkit-overflow-scrolling:touch] [scrollbar-width:none]"
+      :class="showBottomNav ? 'pb-16' : 'pb-3'"
     >
       <slot />
     </div>
-    <BottomNav />
+    <BottomNav v-if="showBottomNav" />
   </div>
 </template>
