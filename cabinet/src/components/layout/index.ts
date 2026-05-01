6e95f85e0a5102a5af270c69e42d2e6180419d47
Mikhail Gusev <majikx.prod0@gmail.com>
2026-05-01 23:00:30 +0500

    refactor

---
+++ cabinet/src/components/layout/index.ts
@@ -0,0 +1,6 @@
+export { default as AppHeader } from './AppHeader.vue'
+export { default as AppLayout } from './AppLayout.vue'
+export { default as AuthLayout } from './AuthLayout.vue'
+export { default as DesktopNavigation } from './DesktopNavigation.vue'
+export { default as MobileNavigation } from './MobileNavigation.vue'
+export { default as Navigation } from './Navigation.vue'
